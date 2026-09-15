13df66c1461c07c89a74fe233a58b65bc894fb0d
Roman Valov <roman.valov@gmail.com>
2026-09-15 23:26:19 +0000

    add: install of copilot cli and cursor cli

--- home/.agents/hooks/vars.sh
+++ home/.agents/hooks/vars.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
+#exec >"$HOME/llmdev/tmp/hook.log" 2>&1
+#set -ex
+
 content="$(cat)"
 
-HARNESS="${1:-"$(cat /proc/$PPID/comm)"}"
+HARNESS="${1:-"$(basename "$(cat /proc/$PPID/cmdline | head -1z)")"}"
 
 {
-	env;
-	echo "${content}";
-	cat /proc/$PPID/comm
+	ps -AFHww;
 	echo;
+	env;
+	echo;
+	pwd;
+	echo;
+	cat /proc/$PPID/comm;
+	cat /proc/$PPID/cmdline | xargs -0n1;
+	basename "$(readlink -f /proc/$PPID/exe)";
+	echo;
+	echo "${content}";
+	echo;
+	echo "$HARNESS";
 } > "$HOME/llmdev/tmp/vars.log"
 
-PROMPT="you are juvelir"
+PROMPT="\
+%agent=$HARNESS
+%cookie=$(uuidgen -r)
+"
 
 case "$HARNESS" in
-	pi)
-		echo "$PROMPT"
-		;;
 	agy)
 		jq -n \
 			--arg prompt "$PROMPT" \
 			'.injectSteps[0].ephemeralMessage = $prompt'
 		;;
-	*)
+	claude|codex)
 		SYSTEM="CONTEXT:DONE"
 		EVENT="$(echo "$content" | jq -r '.hook_event_name')"
 		jq -n \
@@ -30,5 +42,18 @@ case "$HARNESS" in
 			--arg event "$EVENT" \
 			--arg prompt "$PROMPT" \
 			'{systemMessage: $system, hookSpecificOutput: {hookEventName: $event, additionalContext: $prompt}}'
+		;;
+	copilot)
+		jq -n \
+			--arg prompt "$PROMPT" \
+			'{additionalContext: $prompt}'
+		;;
+	cursor)
+		jq -n \
+			--arg prompt "$PROMPT" \
+			'{additional_context: $prompt}'
+		;;
+	*)
+		echo "$PROMPT"
 		;;
 esac
